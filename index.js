91eb213190c9a66005415e7ecca66fd1bb1669aa
MenaVillalobos <jimee69styles@gmail.com>
2023-03-21 14:58:26 -0600

    path is file?

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { link, promises as fs } from 'fs';
+import fs, { link, promises as fsPromises } from 'fs';
 import colors from 'colors';
 import { existsSync } from 'node:fs';
 import  { isAbsolute }  from 'node:path';
@@ -7,26 +7,29 @@ import path from 'path';
 
 // Cómo leer ./archivo.md?
 async function readingFiles(file) {
-  const fileContent = await fs.readFile(file, {encoding: 'utf-8'});
+  const fileContent = await fsPromises.readFile(file, {encoding: 'utf-8'});
   return fileContent;
 }
 export async function mdLinks (pathFile, options) {
   // Validando path
 if (existsSync(pathFile)){
   console.log('THE PATH EXISTS'.rainbow);
-  let data = await readingFiles(pathFile);
   // Path absolute
   if (path.isAbsolute(pathFile)) {
     console.log('THE PATH IS ABSOLUTE!'.bgBlue);
   } else {
-    console.log('SORRY, THIS PATH IS NOT ABSOLUTE):'.bgCyan);
+    console.log('CHANGING RELATIVE PATH TO ABSOLUTE...'.cyan);
     // Turn relative to absolute
     const relativeToAbsolute = path.resolve(pathFile);
-    console.log( 'Your path now is absolute: '.bgMagenta + relativeToAbsolute.magenta);
+    console.log( 'Your path now is absolute: '.bgCyan + relativeToAbsolute.cyan);
   }
-  // Path válido y absoluto entonces lee archivo
+  // Is a path to a file?
+  if (fs.statSync(pathFile).isFile()) {
+    console.log("PATH IS FILE: TRUE".underline.green);
+    let data = await readingFiles(pathFile);
+    // Path válido, absoluto te dirige a un archivo empieza a leerlo
     let elements = data.match(/\[.*?\)/g);
-    if (elements != null && elements.length > 0) {
+    if (elements && elements.length > 0) {
       for (const el of elements){
         let txt = el.match(/\[(.*?)\]/)[1]; // getting txt only
         console.log(txt);
@@ -40,9 +43,14 @@ if (existsSync(pathFile)){
         console.log(urlArray);
       }
     }
+  } else{
+    console.log('THIS IS NOT A FILE, TRY AGAIN'.underline.red);
+  }
+  // const statsObj = fs.statSync(pathFile);
+  // console.log("Path is file:".underline.green ,statsObj.isFile());
+
 } else {
   console.log('PATH NOT FOUND'.red);
 } 
 }
-
 mdLinks(process.argv[2], {});
