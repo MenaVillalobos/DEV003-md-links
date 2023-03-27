ccd07d9a27b3e383570cb3b0405ece8893af6cd5
MenaVillalobos <jimee69styles@gmail.com>
2023-03-27 12:37:14 -0600

    http request

---
+++ cli.js
@@ -0,0 +1,1 @@
+// Aquí va --stats, --validate
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import fs, { link, promises as fsPromises } from 'fs';
+import fs, {promises as fsPromises } from 'fs';
 import colors from 'colors';
 import { existsSync } from 'node:fs';
-import  { isAbsolute }  from 'node:path';
 import path from 'path';
 
-
-// How to read ./file.md?
+// How to read ./file?
 async function readingFiles(file) {
   const fileContent = await fsPromises.readFile(file, {encoding: 'utf-8'});
   return fileContent;
@@ -26,31 +24,31 @@ if (existsSync(pathFile)){
   // Is a path to a file?
   if (fs.statSync(pathFile).isFile()) {
     console.log("PATH IS FILE: TRUE".underline.blue);
-  } else{
-    console.log('THIS IS NOT A FILE, TRY AGAIN'.underline.red);
-  }
-  // Getting file extension
+      // Getting file extension
   if (path.extname(pathFile) === ".md") {
     console.log('THIS IS AN '.yellow + '.md'.bgYellow + ' FILE(:'.yellow);
     let data = await readingFiles(pathFile);
-    // Path is valid, absolut and a path file, so let´s read file
+    // Reading .md file
     let elements = data.match(/\[.*?\)/g);
     if (elements && elements.length > 0) {
       for (const el of elements){
-        let txt = el.match(/\[(.*?)\]/)[1]; // getting txt only
-        console.log(txt);
+        // let txt = el.match(/\[(.*?)\]/)[1]; // getting txt only
+        // console.log(txt);
         let url = el.match(/\((.*?)\)/)[1]; // getting link only
-        console.log(url);
+        // console.log(url);
         const urlArray = []; // empty array to storage the url
-        const linkTxtArray = []; // empty array to storage the file txt
-        linkTxtArray.push(txt);
+        // const linkTxtArray = []; // empty array to storage the file txt
+        // linkTxtArray.push(txt);
         urlArray.push(url);
-        console.log(linkTxtArray);
+        // console.log(linkTxtArray);
         console.log(urlArray);
       }
     }
   } else {
     console.log('THIS IS NOT AN .md FILE, TRY AGAIN'.red.bold);
+  }
+  } else{
+    console.log('THIS IS NOT A FILE, TRY AGAIN'.underline.red);
   }
 } else {
   console.log('PATH NOT FOUND'.red);
