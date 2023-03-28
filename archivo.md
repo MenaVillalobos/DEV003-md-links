a7ad9c98c8a23744599a6ff2741a36dfca8e6df5
MenaVillalobos <jimee69styles@gmail.com>
2023-03-27 19:38:47 -0600

    fixing errors details

--- archivo.md
+++ archivo.md
@@ -2,3 +2,4 @@ Hola no sé qué estoy haciendo
 Pero luzco genial haciéndolo 🤓
 [Esto no es HBO] (https://www.homax.com/mx/es)
 [No es un enlace] (http://arboleess.com/)
+[Esto si es HBO] (https://www.hbomax.com/mx/es)
--- ax.js
+++ ax.js
@@ -5,15 +5,15 @@ export function httpRequest (URL) {
   let answer = "";
   axios.get(URL)
   .then((response) => {
-    answer = "STATUS:".underline.blue + " " + response.status + ' ' + response.statusText.underline.blue
-    console.log(answer);
+    answer = `${"STATUS:".underline.blue} ${response.status} ${response.statusText.underline.blue}`;
+    console.log(`${URL}   ${answer}`);
   }).catch(error => {
     if (error.response) { // status code out of the range of 2xx
-      answer = "Status:".bgRed + " " + error.response.status+ ' ' + error.response.statusText;
-      console.log(answer);
+      answer = `${"STATUS:".underline.red} ${error.response.status} ${error.response.statusText.underline.red}`;
+      console.log(`${URL}   ${answer}`);
     } else if (error.request) { // The request was made but no response was received
       answer = 'The request was made but no response was received'.underline.red;
-      console.log(answer);
+      console.log(`${URL}   ${answer}`);
     } else {// Error on setting up the request
       console.log('Error', error.message);
     }
