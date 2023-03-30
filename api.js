a30999a941e1ca6441a1ef4834577129a8b1e9f8
MenaVillalobos <jimee69styles@gmail.com>
2023-03-29 21:24:42 -0600

    now it works with directory

--- api.js
+++ api.js
@@ -2,34 +2,33 @@ import colors from 'colors';
 import fs, {promises as fsPromises } from 'fs';
 import { existsSync } from 'node:fs';
 import path from 'path';
+import { readingFiles } from './index.js';
 
 // Function validate path
 export const validatePath = (pathFile) => (existsSync(pathFile));
-
 // Function check if path is file
 export const pathIsFile = (pathFile) => (fs.statSync(pathFile).isFile());
 // Function check if is directory
 export const pathIsFolder = (pathFile) => (fs.statSync(pathFile).isDirectory()); 
-// Function reading directory
-const readFolder = (pathFile) => (fs.readdirSync(pathFile)); 
 
 //Creando función para transformar ruta relativa
 export function pathToRelative(pathFile) {
     if (path.isAbsolute(pathFile)) {
         console.log('THE PATH IS ABSOLUTE!'.bgBlue);
-      } else {
+    } else {
         console.log('CHANGING RELATIVE PATH TO ABSOLUTE...'.cyan);
         // Turn relative to absolute
         const relativeToAbsolute = path.resolve(pathFile);
         console.log( 'Your path now is absolute: '.bgCyan + relativeToAbsolute.cyan);
-      }
+    }
 }
 
 // Getting folder's files
-export function getFolderFiles(pathFile) {
+export async function getFolderFiles(pathFile) {
+    const arrFolder = [];
     const folderPath = path.join(pathFile);
     //passsing directoryPath and callback function
-fs.readdir(folderPath, function (err, files) {
+    fs.readdir(folderPath, async function (err, files) {
     //handling error
     if (err) {
         return console.log('Unable to scan directory: ' + err);
@@ -38,12 +37,43 @@ fs.readdir(folderPath, function (err, files) {
     files.forEach(function (file) {
         // Do whatever you want to do with the file 
         if (path.extname(file) === ".md") {
-            console.log('THIS IS AN .md FILE(:'.yellow);
-            const arrFolder = [];
-            const folderFile = arrFolder.push(file);
-            arrFolder.push(pathToRelative(file));
-            console.log(folderFile);
+            console.log('Checking if there is an md file...'.blue);
+            arrFolder.push(pathFile+ "/" + file);
+            console.log(`${file} ${'IS AN MD FILE(:'.magenta}`);
+            const arrTurnAbs = pathToRelative(file);
         } 
     });
+for (let i = 0; i < arrFolder.length; i++) {
+    let data = await readingFiles(arrFolder[i]);
+    console.log('THIS IS YOUR FILE: '.magenta + arrFolder[i]);
+    gettingUrls(data);
+}
 });
 }
+
+// Getting URL's from folder files
+export function gettingUrls(data) {
+    console.log('GETTING URLS...'.bold);
+    let elements = data.match(/\[.*?\)/g);
+    if (elements && elements.length > 0) {
+      const urlArray = []; // empty array to storage the url
+        for (const el of elements){
+       let txt = el.match(/\[(.*?)\]/)[1]; // getting txt only
+        // console.log(txt);
+        let url = el.match(/\((.*?)\)/)[1]; // getting link only
+        // console.log(url);
+        urlArray.push(url);
+        // const linkTxtArray = []; // empty array to storage the file txt
+        // linkTxtArray.push(txt);
+        const linksObject = [];
+        linksObject.push({
+            text: txt,
+            href: url,
+            file: 'necesito la ruta absoluta'
+        })
+        console.log(linksObject);
+        }
+        console.log(urlArray);
+        return {urlArray}
+    } 
+} 
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import fs, {promises as fsPromises } from 'fs';
 import colors from 'colors';
-import { validatePath, pathToRelative, pathIsFile, pathIsFolder, getFolderFiles } from './api.js';
+import { validatePath, pathToRelative, pathIsFile, pathIsFolder, getFolderFiles, gettingUrls} from './api.js';
 import { existsSync } from 'node:fs';
 import path from 'path';
 import { httpRequest } from './ax.js';
 
 // How to read ./file?
-async function readingFiles(file) {
+export async function readingFiles(file) {
   const fileContent = await fsPromises.readFile(file, {encoding: 'utf-8'});
   return fileContent;
 }
@@ -19,52 +19,36 @@ if (validatePath(pathFile)){
   // Is a path to a file?
   if (pathIsFile(pathFile)) {
     console.log("PATH IS FILE: TRUE".underline.blue);
+    // Getting file extension
+    if (path.extname(pathFile) === ".md") {
+      console.log('THIS IS AN '.yellow + '.md'.bgYellow + ' FILE(:'.yellow);
+      let data = await readingFiles(pathFile);
+      gettingUrls(data);
+      
+      }
+    } else if (pathIsFolder(pathFile)) {
+        getFolderFiles(pathFile); // Trae files md con la ruta absoluta
+        console.log('I S   A   F O L D E R'.bgMagenta);
+        gettingUrls(pathFile); // Debería leer archivos y extraer URLS
+        } else{
+        console.log('THIS IS DIRECTORY DOES NOT HAVE VALID FILES, TRY AGAIN'.underline.red);
+      }
+      
+    } else {
+    console.log('THE PATH DOES NOT EXISTS'.red);
+  }
+}
 
-      // Getting file extension
-  if (path.extname(pathFile) === ".md") {
-    console.log('THIS IS AN '.yellow + '.md'.bgYellow + ' FILE(:'.yellow);
-    let data = await readingFiles(pathFile);
-    // Reading .md file
-    let elements = data.match(/\[.*?\)/g);
-    if (elements && elements.length > 0) {
-      const urlArray = []; // empty array to storage the url
-      const linksObject = [];
-      for (const el of elements){
-       let txt = el.match(/\[(.*?)\]/)[1]; // getting txt only
-        // console.log(txt);
-        let url = el.match(/\((.*?)\)/)[1]; // getting link only
-        // console.log(url);
-        urlArray.push(url);
-        // const linkTxtArray = []; // empty array to storage the file txt
-        // linkTxtArray.push(txt);
-        linksObject.push({
-          text: txt,
-          href: url,
-          file: pathFile,
-        });
-      }
-      // options.validate = true:
-      if (options.validate) {
-        for (let i = 0; i < urlArray.length; i++) {
-          httpRequest(urlArray[i]);
-        }
-      }
-      console.log(linksObject);
-    }
-    //AQUI VA CODIGO PARA STATS
-    if (options.stats) {
-      console.log('stats=true');
-    }
-  } else {
-    console.log('THIS IS NOT AN .md FILE, TRY AGAIN'.red.bold);
-  }
-  } else if (pathIsFolder(pathFile)) {
-    getFolderFiles(pathFile);
-    console.log('I S   A   F O L D E R'.bgMagenta);
-  } else{
-    console.log('THIS IS NOT A FILE, TRY AGAIN'.underline.red);
-  }
-  } else {
-  console.log('THE PATH DOES NOT EXISTS'.red);
-}
-}
+//    //AQUI VA CODIGO PARA STATS
+//    if (options.stats) {
+//     console.log('stats=true');
+//     } else {
+//     console.log('THIS IS NOT AN .md FILE, TRY AGAIN'.red.bold);
+//     }
+
+//     // options.validate = true:
+//     if (options.validate) {
+//       for (let i = 0; i < urlArray.length; i++) {
+//         httpRequest(urlArray[i]);
+//       }
+//     }
